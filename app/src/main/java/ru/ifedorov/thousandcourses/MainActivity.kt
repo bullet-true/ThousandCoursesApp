28b5799b4c6ebd9df642dc270a3083ed1bb39509
Igor Fedorov <bullet-1@yandex.ru>
2026-07-09 19:04:45 +0500

    Initial commit

---
+++ app/src/main/java/ru/ifedorov/thousandcourses/MainActivity.kt
@@ -0,0 +1,19 @@
+package ru.ifedorov.thousandcourses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import ru.ifedorov.thousandcourses.ui.theme.ThousandCoursesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThousandCoursesTheme {
+
+            }
+        }
+    }
+}
