8f3a20367c28bc79c83bcc8f095c6811bc57065a
Igor Fedorov <bullet-1@yandex.ru>
2026-07-10 16:22:04 +0500

    TC-4: add app colors

--- app/src/main/java/ru/ifedorov/thousandcourses/MainActivity.kt
+++ app/src/main/java/ru/ifedorov/thousandcourses/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import ru.ifedorov.thousandcourses.ui.theme.ThousandCoursesTheme
+import ru.ifedorov.ui.theme.ThousandCoursesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- core/ui/src/main/java/ru/ifedorov/ui/theme/Color.kt
+++ core/ui/src/main/java/ru/ifedorov/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package ru.ifedorov.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val BackgroundColor = Color(0xFF151515)
+val SurfaceColor = Color(0xFF24252A) // корточки, нижнее меню
+val SurfaceVariantColor = Color(0xFF32333A) // фон полей ввода
+val PrimaryColor = Color(0xFF12B956) // основная кнопка
+val OnPrimaryColor = Color(0xFFF2F2F3) // текст поверх зеленого
+val OnBackgroundColor = Color(0xFFF2F2F3) // основной текст на черном фоне
+val OnSurfaceColor = Color(0xFFECECEF) // текст внутри корточек
+val OnSurfaceVariantColor = Color(0xFFAFAFB4) // вторичный текст
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val OutlineColor = Color(0xFF4D555E) // разделитель
+val VkButtonColor = Color(0xFF2683ED)
+val OkButtonGradientStartColor = Color(0xFFF98509)
+val OkButtonGradientEndColor = Color(0xFFF95D00)
