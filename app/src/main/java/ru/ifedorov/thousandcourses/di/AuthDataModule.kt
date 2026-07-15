8aeb8a07c7fe4cd2519ab56c84fdf2ce4fa8613a
Igor Fedorov <bullet-1@yandex.ru>
2026-07-15 18:25:18 +0500

    TC-17: add auth repository

---
+++ app/src/main/java/ru/ifedorov/thousandcourses/di/AuthDataModule.kt
@@ -0,0 +1,18 @@
+package ru.ifedorov.thousandcourses.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ru.ifedorov.data.repository.FakeAuthRepository
+import ru.ifedorov.domain.repository.AuthRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthDataModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(): AuthRepository = FakeAuthRepository()
+}
